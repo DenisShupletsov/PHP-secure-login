ddcb73ba748d6dc2622c8f41c97b2df21ac0d2a9
DenisShupletsov <shuden99@icloud.com>
2019-07-05 01:45:49 +1100

    Добавил index

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+  echo "<p>login - ".$_POST["login"]."</p>";
+  echo "<p>password - ".$_POST["password"]."</p>";
+
+?>
+
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Project</title>
+ </head>
+ <body>
+
+   <form action="/" method="post">
+
+   <p> <input type="text" name="login" size="10" placeholder="login" required></input></p>
+   <p> <input type="password" name="password" size="10" placeholder="password" required></input></p>
+   <p> <input type="submit" value="enter"></input> </p>
+
+   </form>
+
+ </body>
+</html>
